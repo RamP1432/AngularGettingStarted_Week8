84e0eedc67967395a5c33949b170207fffb4de10
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-03-06 13:52:04 +0530

    [Ram Prakash Pathak] : Ability To launch BridgeLabz Site in a new Tab on clicking BridgeLabz Logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,14 @@ export class AppComponent {
   title = 'HelloWorld';
   message: string = 'Welcome to BridgeLabz';
   logoUrl: string = 'https://www.bridgelabz.com/assets/images/ET-CS-Coding-logo%20.png'; 
+  url = "https://www.bridgelabz.com";
+
+ ngOnInit(): void {
+   this.title = "Hello from BridgeLabz.";
+   }
+onClick($event: any) {
+  console.log("Save button is clicked!", $event);
+  window.open(this.url, "_blank");
+    }  
 }
 
